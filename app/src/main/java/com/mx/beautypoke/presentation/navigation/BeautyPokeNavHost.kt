867acb34b20861a3edf010a55809628b7aa32300
Bedoy <carlos.bedoy@gmail.com>
2026-06-20 16:46:04 -0600

    refactor: use koinViewModel instead of default viewModel() in NavHost

--- app/src/main/java/com/mx/beautypoke/presentation/navigation/BeautyPokeNavHost.kt
+++ app/src/main/java/com/mx/beautypoke/presentation/navigation/BeautyPokeNavHost.kt
@@ -1,12 +1,11 @@
 package com.mx.beautypoke.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.mx.beautypoke.presentation.screen.PokemonDetailScreen
-import com.mx.beautypoke.presentation.viewmodel.PokemonDetailViewModel
+import org.koin.androidx.compose.koinViewModel
 
 object Routes {
     const val POKEMON_DETAIL = "pokemon_detail"
@@ -21,7 +20,7 @@ fun BeautyPokeNavHost(
         startDestination = Routes.POKEMON_DETAIL
     ) {
         composable(Routes.POKEMON_DETAIL) {
-            val viewModel: PokemonDetailViewModel = viewModel()
+            val viewModel = koinViewModel<com.mx.beautypoke.presentation.viewmodel.PokemonDetailViewModel>()
             PokemonDetailScreen(
                 viewModel = viewModel,
                 onBackClick = { /* No previous screen to go back to */ }
